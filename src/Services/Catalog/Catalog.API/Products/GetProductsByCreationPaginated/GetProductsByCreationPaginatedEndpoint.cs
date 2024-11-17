44894a65f53e27cdd2903a282cb6a6032b846d1a
Carlos Mercado <cmercado@rayensalud.com>
2024-11-17 16:53:32 -0300

    Obetener productos paginados incompleto

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCreationPaginated/GetProductsByCreationPaginatedEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.GetProductsByCreationPaginated
+{
+    public class GetProductsByFilterRequest
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public class GetProductsByCreationPaginatedEndpoint
+    {
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCreationPaginated/GetProductsByCreationPaginatedHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Products.GetProductsByCreationPaginated
+{
+    public class GetProductsByCreationPaginatedHandler
+    {
+    }
+}
